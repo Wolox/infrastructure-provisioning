d920bee5dd84de595b60536ccbec1cb27f97e282
Matias De Santi <matias.desanti@wolox.com.ar>
2017-01-23 16:44:51 -0300

    Rubocop fixes

--- cli/aws.rb
+++ cli/aws.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
-require "rubygems"
+require 'rubygems'
 require 'thor'
 require_relative '../core/backend'
 
 module Cli
   class Aws < Thor
-    desc "create_eb_application project", "creates an Elastic Beanstalk application"
+    desc 'create_eb_application project', 'creates an Elastic Beanstalk application'
     option :profile, required: true
     option :region
     option :environment_name
@@ -14,7 +14,7 @@ module Cli
       Core::Backend.new(project, options).create
     end
 
-    desc "Prints available stacks", "Prints available stacks"
+    desc 'Prints available stacks', 'Prints available stacks'
     option :profile, required: true
     def show_backend_stacks
       Core::Backend.new('', options).show_stacks
