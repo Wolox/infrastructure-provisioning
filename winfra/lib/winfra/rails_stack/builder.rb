b5c385eeff4f5884ec0b852208df1aef73e7cc42
Matias De Santi <matias.desanti@wolox.com.ar>
2017-03-17 19:03:10 -0300

    Adds vpc support

--- winfra/lib/winfra/rails_stack/builder.rb
+++ winfra/lib/winfra/rails_stack/builder.rb
@@ -3,7 +3,8 @@ require "erb"
 module Winfra
   module RailsStack
     class Builder
-      attr_reader :path, :env, :public_website, :app_name, :beanstalk_base, :rds_base, :has_vpc
+      attr_reader :path, :env, :public_website, :app_name, :beanstalk_base, :rds_base, :has_vpc,
+                  :vpc_base
 
       BEANSTALK_RESOURCE_TEMPLATE = Winfra.path_to('winfra/templates/beanstalk-resource.tf.erb')
       BEANSTALK_SG_TEMPLATE = Winfra.path_to('winfra/templates/beanstalk-sg.tf.erb')
@@ -11,6 +12,9 @@ module Winfra
 
       RDS_RESOURCE_TEMPLATE = Winfra.path_to('winfra/templates/rds-resource.tf.erb')
       RDS_SG_TEMPLATE = Winfra.path_to('winfra/templates/rds-sg.tf.erb')
+
+      VPC_RESOURCE_TEMPLATE = Winfra.path_to('winfra/templates/vpc-resource.tf.erb')
+      VPC_OUTPUTS_TEMPLATE = Winfra.path_to('winfra/templates/vpc-outputs.tf.erb')
 
       MAIN_TEMPLATE = Winfra.path_to('winfra/templates/rails-stack-main.tf.erb')
 
@@ -21,24 +25,32 @@ module Winfra
         @app_name = app_name
         @beanstalk_base = "#{path}/infrastructure/modules/beanstalk"
         @rds_base = "#{path}/infrastructure/modules/rds"
+        @vpc_base = "#{path}/infrastructure/modules/vpc"
         @profile = profile
       end
 
       def build
-        FileUtils.mkdir_p(beanstalk_base)
-        FileUtils.mkdir_p(rds_base)
         generate_beanstalk_templates
         generate_rds_templates
+        generate_vpc_templates if has_vpc
         generate_main_template
       end
 
+      def generate_vpc_templates
+        FileUtils.mkdir_p(vpc_base)
+        render_template(VPC_RESOURCE_TEMPLATE, "#{vpc_base}/vpc.tf")
+        render_template(VPC_OUTPUTS_TEMPLATE, "#{vpc_base}/outputs.tf")
+      end
+
       def generate_beanstalk_templates
+        FileUtils.mkdir_p(beanstalk_base)
         render_template(BEANSTALK_RESOURCE_TEMPLATE, "#{beanstalk_base}/beanstalk.tf")
         render_template(BEANSTALK_SG_TEMPLATE, "#{beanstalk_base}/beanstalk-sg.tf")
         render_template(BEANSTALK_OUTPUTS_TEMPLATE, "#{beanstalk_base}/outputs.tf")
       end
 
       def generate_rds_templates
+        FileUtils.mkdir_p(rds_base)
         render_template(RDS_RESOURCE_TEMPLATE, "#{rds_base}/rds.tf")
         render_template(RDS_SG_TEMPLATE, "#{rds_base}/rds-sg.tf")
       end
